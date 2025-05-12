c66f82368daba976c18db3303bc1fd97616c7ba8
Bobo <109029622+Bobo5345@users.noreply.github.com>
2025-05-12 11:42:27 +0530

    Add files via upload

---
+++ number_to_digit_sum.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Initializing variables and array
+    int number[11];
+    int sum = 0, temp = 0, count = 0;
+
+    // Asking user to input the number
+    cout << "\nEnter the number you need to calculate, digit by digit.";
+    cout << "\nTo stop, enter -1.";
+
+    // For loop to input
+    for (int i = 0; i < 11; i++)
+    {
+        cout << "\nEnter digit " << i+1 << " : ";
+        cin >> temp;
+
+        // For stopping at -1
+        if (temp == -1)
+         break;
+        
+
+        number[i] = temp;
+        count++;
+        temp = 0;
+    }
+
+    // Now taking sum
+    temp = 0;
+    for (int j = 0; j < count; j++) 
+    {
+        temp = number[j];
+        sum = sum + temp;
+    }
+
+    // Outputting sum
+    cout << "\nThe sum is: " << sum;
+
+    return 0;
+}
